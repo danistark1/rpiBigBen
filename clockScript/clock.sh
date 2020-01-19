8a1246f006d3f6e96167f0c98162ba34392be169
Dani <31298322+danimajdalani@users.noreply.github.com>
2020-01-19 11:45:42 -0500

    update player name

--- clockScript/clock.sh
+++ clockScript/clock.sh
@@ -3,15 +3,15 @@
 minute=$(date +"%M")
 if [[ $minute == 30 ]]
 then
-mplayer "/home/pi/big-ben/FOPGM68INC0CAI9.mp3"
+mpv "/home/pi/big-ben/bigbenstrikes.mp3"
 elif [[ $minute == 00 ]]
 then
-mplayer "/home/pi/big-ben/hourlychimebeg.mp3"
+mpv "/home/pi/big-ben/hourlychimebeg.mp3"
 hour=$(date +"%I")
 x=0
 while [ $x -lt $hour ]
 do
-mplayer "/home/pi/big-ben/bigbenstrikes.mp3"
+mpv "/home/pi/big-ben/bigbenstrikes.mp3"
 let x=x+1
 done
 fi
